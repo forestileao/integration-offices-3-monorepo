bf3d884cec1b0696703296a33907a63c1376d8dd
Pedro Leão <pedroforestileao@gmail.com>
2024-12-14 17:46:08 -0300

    fix circular import

--- firmware/temp_humidity.py
+++ firmware/temp_humidity.py
@@ -2,9 +2,6 @@ from RPi import GPIO
 from SDL_Pi_HDC1080 import SDL_Pi_HDC1080
 from multiplexer import Multiplexer
 
-from main import chambers
-
-
 class TempHumidity:
   def __init__(self, chambers=[], multiplexer = None) -> None:
     self.multiplexer = multiplexer
@@ -58,10 +55,56 @@ class TempHumidity:
         GPIO.output(chamber['peltierPin'], GPIO.LOW)
 
 if __name__ == '__main__':
-    multiplexer = Multiplexer()
-    temp = TempHumidity(chambers, multiplexer=multiplexer)
+  chambers = [
+    {
+      'id': '90617ba4-ee9b-488f-82bc-cbe8b43aac67',
+      'whitePin': 17,
+      'ledPin': 27,
+      'pumpPin': 18,
+      'heaterPin': 22,
+      'peltierPin': 23,
+      'chamberLocation': 1200,
+      'waterLevelChannel': 0,
+      'soilMoistureChannel': 1,
+      'ledLightsActivated': False,
+      'tempMuxChannel': 0,
+      'fanPin': 24,
+      'parameters': {
+        "temperatureRange": "17",
+        "soilMoistureLowerLimit": 60,
+        "photoCaptureFrequency": "60",
+        "id": "b231822f-5e74-41ea-9678-0c61404fe6dd",
+        "lightingRoutine": "07:40/18:20",
+        "ventilationSchedule": "10:00/11:00"
+      }
+    },
+    {
+      'id': '7ce04bef-2212-4a9b-8262-ed659cd124ab',
+      'whitePin': 22,
+      'ledPin': 23,
+      'pumpPin': 24,
+      'heaterPin': 25,
+      'peltierPin': 8,
+      'chamberLocation': 4200,
+      'waterLevelChannel': 2,
+      'soilMoistureChannel': 3,
+      'ledLightsActivated': False,
+      'tempMuxChannel': 1,
+      'fanPin': 9,
+      'parameters': {
+        "temperatureRange": "28",
+        "soilMoistureLowerLimit": 60,
+        "photoCaptureFrequency": "60",
+        "id": "1e43809c-0daa-413f-ab18-988ef80e4af6",
+        "lightingRoutine": "07:40/18:20",
+        "ventilationSchedule": "10:00/11:00"
+        }
+    },
+  ]
+  multiplexer = Multiplexer()
+  temp = TempHumidity(chambers, multiplexer=multiplexer)
 
 
-    for chamber in chambers:
-      temp.read_temperature(chamber['id'])
-      temp.read_humidity(chamber['id'])
+  for chamber in chambers:
+    temp.read_temperature(chamber['id'])
+    temp.read_humidity(chamber['id'])
